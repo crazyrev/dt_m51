7c2b574ae9ad158f304e8a2f55fbff902bf82b34
havocuser19 <rajatbillava2001@gmail.com>
2022-04-05 15:13:55 +0200

    Pixys BringUp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020 The pixysOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_m51.mk
+    $(LOCAL_DIR)/pixys_m51.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_m51-user \
-    lineage_m51-userdebug \
-    lineage_m51-eng
+    pixys_m51-user \
+    pixys_m51-userdebug \
+    pixys_m51-eng
