12bf071e026ffe75ab2f794a1edb34deaea1b679
Silent <68320581+havocuser19@users.noreply.github.com>
2022-05-10 10:11:47 +0530

    Oops 😬already declared

--- device.mk
+++ device.mk
@@ -18,12 +18,6 @@ DEVICE_PATH := device/samsung/m51
 
 # Prop
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.has_wide_color_display=true \
-    ro.surface_flinger.has_HDR_display=true \
-    ro.surface_flinger.use_color_management=true \
-    ro.surface_flinger.wcg_composition_dataspace=143261696 \
-    ro.surface_flinger.protected_contents=true \
-    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.secure=0 \
     ro.adb.secure=0
     persist.service.acm.enable=0 \
